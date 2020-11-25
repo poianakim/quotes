de8068a6afc8055022a244d3643764163f2aedfa
kby906 <kby906@gmail.com>
2020-11-25 10:20:55 +0100

    key error fixed

--- src/fbase.js
+++ src/fbase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 var firebaseConfig = {
-    apiKey: process.env.API_KEY,
+    apiKey: "AIzaSyAef8qIjnu82MunJScf7VzoLq1F2bm9YQo",
     authDomain: `${process.env.PROJECT_ID}.firebaseapp.com`,
     databaseURL: `https://${process.env.PROJECT_ID}.firebaseio.com`,
     projectId: process.env.PROJECT_ID,
